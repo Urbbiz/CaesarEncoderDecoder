ac17cc524fb47c3b04f1194f8380e2aa8bcbb5b4
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2022-01-30 11:39:13 +0200

    main logic added

---
+++ CaesarEncoderDecoder/CaesarEncoderDecoderSrc/Services/CaesarChiperService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarEncoderDecoder.CaesarEncoderDecoderSrc.Services
+{
+    public class CaesarChiperService
+    {
+        public string GetEncodedString(string input, int key)
+        {
+            string encodedString = string.Empty;
+
+            foreach (char character in input)
+
+                encodedString += GetEncodedCharacter(character, key);
+
+            return encodedString;
+        }
+
+        public string GetDecodedString(string encodedString, int key)
+        {
+            return GetEncodedString(encodedString, 26 - key);
+        }
+
+        private char GetEncodedCharacter(char character, int key)
+        {
+            if (!char.IsLetter(character))
+                return character;
+
+            char offset = char.IsUpper(character) ? 'A' : 'a';
+            return (char)((((character + key) - offset) % 26) + offset);
+        }
+    }
+}
